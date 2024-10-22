4979fb033818f5b1671bd183d90dbf8a96b06ab7
Jklim12 <julianova1987@gmail.com>
2024-10-22 13:47:50 +0300

    Add files via upload

---
+++ modyl_1_5.py
@@ -0,0 +1,8 @@
+#immutable_var=1,2,'s','t'
+#print(immutable_var)#
+#immutable_var[0]=200#потому,что картежи являются неизменяемыми структурами#
+
+
+mutable_list=([1,2,3],0)
+mutable_list[0][0]=2
+print(mutable_list)
